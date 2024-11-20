c7c9b56e4e5c9debdd9c97dac22f3481f0571bc4
rafissq <148448594+rafissq@users.noreply.github.com>
2024-11-20 11:08:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Arte',
+    'O que é "hachuras"?',
+    'Hachuras são linhas paralelas que criam tonalidades e texturas em desenhos.'
+)
+
+criaCartao(
+    'Design Gráfico',
+    'Como reverter uma ação feita no photoshop?',
+    'Pressionando CTRL + Z'
+)
+
+criaCartao(
+    'Sociologia',
+    'O que é patriarcalismo?',
+    'O patriarcalismo é uma forma de construção social baseada no patriarcado, que é o domínio social ou uma estrutura de poder social centralizada no homem ou no masculino'
+)
+
+criaCartao(
+    'Vôlei',
+    'Qual a única posição do volei exclusivamente defensiva?',
+    'O líbero tem uma função exclusivamente defensiva no vôlei, sendo responsável por garantir a estabilidade da recepção e da defesa da equipe.'
+)
